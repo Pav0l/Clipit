fcddacc23310a3586ecbeb2f61ae621f07dcd251
Pav0l <pavol.karas@gmail.com>
2022-02-12 19:50:36 +0100

    hardhat: clean up integration tests and add another scenario

--- hardhat/test/integration.test.ts
+++ hardhat/test/integration.test.ts
@@ -9,11 +9,11 @@ import { AuctionHouse, WETH } from "../typechain";
 import { expect } from "chai";
 import { Decimal, generateSignature } from "../lib";
 
-describe("AuctionHouse", function () {
+describe.only("AuctionHouse", function () {
   let tokenContract: ClipIt;
   let marketContract: Market;
   let auctionContract: AuctionHouse;
-  let deployer: Wallet, owner: Wallet, three: Wallet, four: Wallet;
+  let deployer: Wallet, owner: Wallet, bidder: Wallet, bidder2: Wallet;
   let wethContract: WETH;
   let currencyContract: BaseERC20;
   let wethAddress: string;
@@ -73,11 +73,16 @@ describe("AuctionHouse", function () {
       "0",
       currency
     );
-    return auctionCaller;
+  }
+
+  async function getAndApproveWeth(caller: Wallet, to: string, amount = oneEther) {
+    const wethCaller = wethContract.connect(caller);
+    await wethCaller.deposit({ value: amount });
+    await wethCaller.approve(to, oneEther);
   }
 
   beforeEach(async () => {
-    [deployer, owner, three, four] = waffle.provider.getWallets();
+    [deployer, owner, bidder, bidder2] = waffle.provider.getWallets();
 
     const marketFactory = await ethers.getContractFactory("Market", {
       signer: deployer,
@@ -97,82 +102,110 @@ describe("AuctionHouse", function () {
       signer: deployer,
     });
     auctionContract = (await auctionFactory.deploy(tokenContract.address, wethAddress)) as AuctionHouse;
+
+    // mint and approve for auction
+    await setupToken(owner);
   });
 
   it("auction in weth", async () => {
-    // mint and approve for auction
-    await setupToken(owner);
-
-    const auctionCaller = await setupAuction(wethAddress);
+    await setupAuction(wethAddress);
 
     // swap & approve WETH
-    const wethCaller = wethContract.connect(three);
-    await wethCaller.deposit({ value: oneEther });
-    await wethCaller.approve(auctionContract.address, oneEther);
+    await getAndApproveWeth(bidder, auctionContract.address);
+
     // bid
-    const bidder = auctionContract.connect(three);
-    await bidder.createBid("0", oneEther, { value: oneEther });
+    await auctionContract.connect(bidder).createBid("0", oneEther, { value: oneEther });
 
     await ethers.provider.send("evm_increaseTime", [oneDay]);
 
-    await auctionCaller.endAuction("0");
+    await auctionContract.connect(owner).endAuction("0");
 
-    const ownerBalance = await wethCaller.balanceOf(owner.address);
+    const ownerBalance = await wethContract.balanceOf(owner.address);
     // owner === creator => ownerShare + creatorShare - deployerShare => 1 * (0.9 + 0.1 - 0.9*0.01) = 0,991ETH
     expect(ownerBalance.toString()).eql(oneEther.mul(991).div(1000).toString());
 
-    const bidderBalance = await wethCaller.balanceOf(bidder.address);
+    const bidderBalance = await wethContract.balanceOf(bidder.address);
     expect(bidderBalance.toString()).eql(BigNumber.from(0).toString());
-    expect(await tokenContract.ownerOf(tid)).eql(three.address);
+    expect(await tokenContract.ownerOf(tid)).eql(bidder.address);
   });
 
   it("auction in erc20", async () => {
-    await setupToken(owner);
-
-    const oneEther = ethers.utils.parseEther("1");
-
     // setup ERC20 token, mint and approve
-    await deployCurrency(three);
-    await currencyContract.mint(three.address, oneEther);
-    const erc20Caller = currencyContract.connect(three);
+    await deployCurrency(bidder);
+    await currencyContract.mint(bidder.address, oneEther);
+    const erc20Caller = currencyContract.connect(bidder);
     await erc20Caller.approve(auctionContract.address, oneEther);
 
-    const auctionCaller = await setupAuction(currencyContract.address);
+    await setupAuction(currencyContract.address);
 
     // bid
-    const bidder = auctionContract.connect(three);
-    await bidder.createBid("0", oneEther, { value: oneEther });
+    await auctionContract.connect(bidder).createBid("0", oneEther, { value: oneEther });
 
     // move forward in time until auction expires
     await ethers.provider.send("evm_increaseTime", [oneDay]);
 
-    await auctionCaller.endAuction("0");
+    await auctionContract.connect(owner).endAuction("0");
 
     const balance = await currencyContract.balanceOf(owner.address);
     expect(balance.toString()).eql(oneEther.mul(991).div(1000).toString());
 
     const bidderBalance = await currencyContract.balanceOf(bidder.address);
     expect(bidderBalance.toString()).eql(BigNumber.from(0).toString());
-    expect(await tokenContract.ownerOf(tid)).eql(three.address);
+    expect(await tokenContract.ownerOf(tid)).eql(bidder.address);
   });
 
   it("auction in native ETH", async () => {
-    await setupToken(owner);
-    const auctionCaller = await setupAuction(ethers.constants.AddressZero);
+    await setupAuction(ethers.constants.AddressZero);
 
     // bid
-    await auctionContract.connect(three).createBid("0", oneEther, { value: oneEther });
+    await auctionContract.connect(bidder).createBid("0", oneEther, { value: oneEther });
 
     // move forward in time until auction expires
     await ethers.provider.send("evm_increaseTime", [oneDay]);
 
     const ownerBalanceBefore = await owner.getBalance();
 
-    await auctionCaller.endAuction("0");
+    await auctionContract.connect(owner).endAuction("0");
 
     const ownerBalanceAfter = await owner.getBalance();
     // just make sure balance increased
     expect(ownerBalanceAfter.gt(ownerBalanceBefore)).eql(true);
-    expect(await tokenContract.ownerOf(tid)).eql(three.address);
+    expect(await tokenContract.ownerOf(tid)).eql(bidder.address);
+  });
+
+  it("market bid persists through auction cycle from active to finished", async () => {
+    // get WETH and approve Market contract
+    await getAndApproveWeth(bidder, marketContract.address);
+
+    const marketBid = {
+      amount: oneEther,
+      currency: wethAddress,
+      bidder: bidder.address,
+      recipient: bidder.address,
+      sellOnShare: Decimal.from(0),
+    };
+    // set market bid
+    await tokenContract.connect(bidder).setBid(tid, marketBid);
+
+    //
+    await setupAuction(wethAddress);
+
+    // get WETH and approve auction for second bidder
+    await getAndApproveWeth(bidder2, auctionContract.address);
+    // bid on auction as bidder2
+    await auctionContract.connect(bidder2).createBid("0", oneEther, { value: oneEther });
+
+    await ethers.provider.send("evm_increaseTime", [oneDay]);
+    await auctionContract.connect(owner).endAuction("0");
+
+    // auction ended -> bidder2 is owner of token
+    const newOwner = await tokenContract.ownerOf(tid);
+    expect(newOwner).eql(bidder2.address);
+
+    // bidder2 can now accept bid
+    await tokenContract.connect(bidder2).acceptBid(tid, marketBid);
+
+    const latestOwner = await tokenContract.ownerOf(tid);
+    expect(latestOwner).eql(bidder.address);
   });
 });
