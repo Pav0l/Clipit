4904faa4eba46dd31ed182284e490ec1d1dc2d6e
Pav0l <pavol.karas@gmail.com>
2022-02-20 21:31:09 +0100

    extension: create dummy extension app

--- app/README.md
+++ app/README.md
@@ -11,3 +11,10 @@ Run:
 ### Configuration
 
 Apps configuration is based on [node-config](https://github.com/lorenwest/node-config) package, which loads proper configuration file based on `NODE_ENV` variable (e.g. tests run with `NODE_ENV=test` so it loads config from `./config/test.json`)
+
+# ClipIt Twitch extension
+
+- generate certificates via `mkcert` into `./certs` folder: `mkcert -install -cert-file certs/cert.pem -key-file certs/key.p em localhost`
+- run `yarn start:ext`
+
+Open the extension in Chrome or inside Twitch Developer Rig
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,11 +1,16 @@
 const path = require("path");
+const fs = require("fs");
 const config = require("config");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const isDevelopment = process.env["NODE_ENV"] === "development";
+const mode = process.env["APP_MODE"]; // app / extension
 
-const webpackConfig = {
+console.log(`Running Webpack in ${mode} mode`);
+
+
+const appConfig = {
   entry: "./src/index.tsx",
   output: {
     path: path.join(__dirname, "/dist"),
@@ -62,4 +67,74 @@ const webpackConfig = {
   ],
 };
 
-module.exports = webpackConfig;
+const extensionConfig = {
+  entry: "./extension/index.tsx",
+  output: {
+    path: path.join(__dirname, "/ext-dist"),
+    filename: "index.js",
+    publicPath: "/", // react-router default path
+  },
+  devServer: {
+    port: 3001,
+    watchContentBase: true,
+    historyApiFallback: true,
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem'),
+    }
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./extension/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "config.html",
+      template: "./extension/public/config.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "panel.html",
+      template: "./extension/public/panel.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "streamer.html",
+      template: "./extension/public/streamer.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "viewer.html",
+      template: "./extension/public/viewer.html",
+    }),
+    new webpack.DefinePlugin({
+      CONFIG: JSON.stringify({ ...config, isDevelopment }),
+    }),
+  ],
+};
+
+
+module.exports = mode === "extension" ? extensionConfig : appConfig;
